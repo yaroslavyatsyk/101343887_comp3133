d2321bac884f7f12ae221684ead58befb4d1d4c4
Yaroslav9728 <yaroslav9728@gmail.com>
2023-01-17 10:34:35 -0500

    Initial commit

---
+++ Lab01_101343887_comp3133/index.js
@@ -0,0 +1,12 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+
+
+fs.createReadStream('input_countries.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
