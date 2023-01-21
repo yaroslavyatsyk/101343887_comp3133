ec8b5403aa2e07a3ba3a6c5375fe80409e983556
Yaroslav9728 <yaroslav9728@gmail.com>
2023-01-21 15:28:54 -0500

    added lab ex1

--- Lab01_101343887_comp3133/index.js
+++ Lab01_101343887_comp3133/index.js
@@ -1,12 +1,36 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
+console.log("Removing canada.txt file if exists");
+if (fs.existsSync('canada.txt')) {
+    fs.unlinkSync('canada.txt');
+    console.log('canada.txt is deleted');
+}
+
+console.log("Removing usa.txt file if exists");
+if (fs.existsSync('usa.txt')) {
+    fs.unlinkSync('usa.txt');
+    console.log('usa.txt file is deleted');
+}
+
+fs.appendFileSync('canada.txt', `country,year,population\n`);
+fs.appendFileSync('usa.txt', `country,year,population\n`);
 
 fs.createReadStream('input_countries.csv')
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row);
+    console.log(row)
+
+
+    if(row.country == 'Canada') {
+        fs.appendFileSync('canada.txt',`${row.country},${row.year},${row.population}\n`)
+    }
+    if(row.country == 'United States') {
+      fs.appendFileSync('usa.txt',`${row.country},${row.year},${row.population}\n`)
+    }
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
   });
+
+  
