115a6770aa2768209477b76785d950ef9570d233
Yaroslav9728 <yaroslav9728@gmail.com>
2023-02-11 17:14:12 -0500

    intial commit

---
+++ lab4_users_database/models/users.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        minLength: 4
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validation: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
+    address: {
+        street: {
+            type: String,
+            required: true
+        },
+        suite: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true,
+            match: /^[a-zA-Z\s]+$/
+        },
+        zipcode: {
+            type: String,
+            required: true,
+            match: /^\d{5}-\d{4}$/
+        }
+    },
+    geo: {
+        lat: {
+            type: Number,
+            required: true
+        },
+        lng: {
+            type: Number,
+            required: true
+        }
+    },
+    phone: {
+        type: String,
+        required: true,
+        match: /^1-\d{3}-\d{3}-\d{4}$/
+    },
+    website: {
+        type: String,
+        required: true,
+        match: /^https?:\/\/.+/
+    },
+    company: {
+        name: {
+            type: String,
+            required: true
+        },
+        catchPhrase: {
+            type: String,
+            required: true
+        },
+        bs: {
+            type: String,
+            required: true
+        }
+    }
+
+})
+
+const Users = mongoose.model("Users",userSchema)
+module.exports = Users;
